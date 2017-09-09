cd92d2781a8d3f14d90145d567807c41ce0ce560
sophiasun0515 <ruixuansun@gatech.edu>
2017-09-09 11:42:19 -0400

    Location java file

---
+++ Location.java
@@ -0,0 +1,64 @@
+
+public class Location {
+
+    /** instances needed */
+
+    public boolean isStation = false;
+    private int xValue;
+    private int yValue;
+    private int pFlow;
+    private int[] RGB;
+
+    /** Constructor */
+    public Location(int xValue, int yValue) {
+        this.xValue = xValue;
+        this.yValue = yValue;
+        RGB = new int[3];
+
+    }
+
+    /** Bunch of getters and setters */
+    public int getpFlow() {
+        return pFlow;
+    }
+
+    public void setpFlow(int flow) {
+        pFlow = flow;
+    }
+
+    public int getRGB() {
+        return RGB;
+    }
+
+    public void setRGB(int RGB) {
+        this.RGB = RGB;
+    }
+
+    /** check if the current location is a station
+     *
+     * @param x the x value of station
+     * @param y the y value of station
+     * @return if the current location is station
+     */
+    public boolean checker(int x, int y) {
+        if (x == xValue && y == yValue) {
+            isStation = true;
+        }
+        return isStation;
+    }
+
+    /** return the proper RGB value for current location
+     *
+     * @param red the red value for RGB
+     * @param green the green value for RGB
+     * @param blue the blue value for RGB
+     * @return the
+     */
+    public int[] getLocationRGB(int red, int green, int blue) {
+        if (isStation) {
+            RGB[0] = 255;
+        }
+        return RGB;
+    }
+
+}
